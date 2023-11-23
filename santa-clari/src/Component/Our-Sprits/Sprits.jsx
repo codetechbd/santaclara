6170a9704325fc99e63ea8e4e6a0015a6a657146
Shakib <shakibc83@gmail.com.com>
2023-11-24 00:04:50 +0600

    update

---
+++ santa-clari/src/Component/Our-Sprits/Sprits.jsx
@@ -0,0 +1,63 @@
+import './Sprits.css'
+// import sc from "../../img/sc.jpg"
+import sc1 from "../../img/sc1.png"
+import boltol1 from "../../img/btl (1).png"
+// import boltol2 from "../../img/btl (2).png"
+// import boltol3 from "../../img/btl (3).png"
+import boltol4 from "../../img/btl (4).png"
+// import boltol5 from "../../img/btl (5).png"
+// import boltol6 from "../../img/btl (6).png"
+import boltol7 from "../../img/btl (7).png"
+const Sprits = () => {
+    return (
+        <div id='spritss' className=' px-10 lg:px-20   py-20 font-medium bgclr text-white' >
+
+            <h1 className='text-2xl text-justify '> <span className='font-black text-2xl'> Santa clara Whisky</span>  – Aged for a minimum 3 years in White French Oak Barrels. Created from a mash bill
+                of 50% corn, 40% sugar cane, 50% and 10% rye, this whisky is fantastic for sipping. Santa clara has notes
+                of maple and dried fruit on the nose carry over into a palate filled with caramel and a pleasant robust
+                oakiness. Finishing off with a slight hint of pepper and baking spices..
+
+                <img className='h-64 lg:h-screen' src={sc1} alt="" srcset="" />
+                <img className=' lg:h-screen mx-auto' src={boltol1} alt="" srcset="" />
+                {/* <img className=' lg:h-screen mx-auto' src={boltol2} alt="" srcset="" />
+                <img className=' lg:h-screen mx-auto' src={boltol3} alt="" srcset="" /> */}
+                {/* <div className='lg:flex' >
+                        <img className=' lg:h-screen mx-auto w-2/3'  src={boltol4} alt="" srcset="" />
+                        <img className=' lg:h-screen mx-auto w-1/2' src={boltol5} alt="" srcset="" />
+                </div> */}
+               
+<div class="flex item-center content-center" >
+New choice, Master edition
+                                    <br/> Extra smooth......
+                                    <br/> SALADO VODKA
+</div>
+                                                    
+
+                <img className=' lg:h-screen mx-auto' src={boltol7} alt="" srcset="" /> 
+               
+                invite you to indulge in life&#39;s luxury. With meticulous craftsmanship and attention to detail,
+Purity delivers unparalleled quality. Each sip of our spirits is a symphony of flavors,
+elevating any occasion. Embrace your self-worth and savor the extraordinary. With Purity,
+every celebration becomes a testament to your worth. Cheers to opulence and
+unforgettable experiences with Purity by your side. You´re worth it. Each vodka we distill is a
+surprisingly complex sip of premium ingredients, distilled to bring you a truly unique blend of taste and
+tradition. Each recipe is meticulously developed through extensive research, trial and error, and the
+enjoyment of sampling the best vodkas available. Recipes were developed through extensive research,
+distillery visits, tastings, trial and error, and of course lots of fun drinking. only organic vodka made from
+Cuban grain and a variety of core and specialty spirits. Milled, mashed, and fermented from scratch
+using only the finest ingredients, Santa Clara products speak to our core values: sourcing locally,
+drinkability, high quality, and value. <br /> <br />
+                <br/>
+                “Without a doubt this is a moment of growth for the brand's portfolio, and in this
+                sense we promote a product with an identity and tradition that make it unique. This is
+                how we intend to reach more audiences who are looking for that unique flavor in the
+                cradle of whisky in Cuba. For santa clara whisky, the work with Diageo is aimed at
+                making visible and making known to the world the peculiarities and quality of Cuban
+                whisky. We are very satisfied with the path we have created”
+                <br />
+                Upcoming (Apple pie)</h1>
+        </div >
+    );
+};
+
+export default Sprits;
